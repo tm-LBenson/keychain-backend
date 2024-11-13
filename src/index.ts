2b8e0d0b8933870fbb18aae1668bbc952b2174e0
Lewis Benson <tm.lewisbenson@gmail.com>
2024-11-13 08:23:34 -0500

    add reference id

--- src/index.ts
+++ src/index.ts
@@ -89,6 +89,7 @@ const createOrder = async (cart: Item[]): Promise<OrderResponse> => {
     cart.map(async (item) => {
       const itemDetails = await fetchProductDetails(item.id);
       return {
+        reference_id: item.id,
         amount: {
           currencyCode: itemDetails?.unitAmount.currencyCode,
           value: itemDetails?.unitAmount.value,
